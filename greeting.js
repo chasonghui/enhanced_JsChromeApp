467641a2102804d84dc3fc76a3bbdbaf39480d2e
chasonghui <chasonghui@naver.com>
2021-02-08 02:52:32 +0000

    background, css

--- greeting.js
+++ greeting.js
@@ -24,13 +24,14 @@ function askForName() {
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+  greeting.innerText = `Hi ${text}`;
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     askForName();
+    
   } else {
     paintGreeting(currentUser);
   }
--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector(".js-weather");
 
-const API_KEY = "9283589f72c8b746d51c4c6d702c1a38";//YOUR API_KEY
+const API_KEY = "7e477f605c19fe3a025d257043b559d3";//YOUR API_KEY
 const COORDS = 'coords';
 
 function getWeather(lat, lng) {
@@ -8,25 +8,31 @@ function getWeather(lat, lng) {
     return reponse.json();
   }).then(function (json) {
     console.log(json);
-    const temperature = json.main.temp;
-    const place = json.name;
-    weather.innerText = `${temperature} @ ${place}`;
+    let temperature = json.main.temp;
+    let feels_like=json.main.feels_like;
+    let place = json.name;
+    let temp_max=json.main.temp_max;
+    let temp_min=json.main.temp_min;
+    //let icon=json.weather[0].icon;
+    weather.innerText = `${place}\n${temperature}/${temp_min}°\n체감온도:${feels_like}°\n`;
   });
 }
 
 function saveCoords(coordsObj) {
-  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+ // localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSucces(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  const coordsObj = {
+  
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let coordsObj = {
     latitude: latitude,
     longitude: longitude
   };
+  console.log(coordsObj);
   saveCoords(coordsObj);
-  getWeather(latitue, longitude);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -35,15 +41,15 @@ function handleGeoError() {
 
 function askForCoords() {
   //getCurrentPosition -> watchPosition으로 변경함 
-  navigator.geolocation.watchPosition(handleGeoSucces, handleGeoError);
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
 
 function loadCoords() {
-  const loadedCoords = localStorage.getItem(COORDS);
+  let loadedCoords = localStorage.getItem(COORDS);
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    const parsedCoords = JSON.parse(loadedCoords);
+    let parsedCoords = JSON.parse(loadedCoords);
     getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
